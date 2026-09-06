09d8b2b57b11d93f5b237a5a62feac6064c235aa
AbhishekSaini9520 <abhisheksaini200404@gmail.com>
2026-09-06 13:38:51 +0530

    Interacting with other contract

---
+++ Basics/09_vendingMachine.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "./09_IsodaPrice.sol";
+
+contract vendingMachine{
+   
+   uint soda;
+   address public owner;
+   IsodaPrice public priceOracle;
+
+
+   constructor(address _addressOracle){
+        soda = 100;
+        owner = msg.sender;
+        priceOracle = IsodaPrice(_addressOracle);
+   }
+
+   function getBalance() public view returns(uint){
+     
+     return address(this).balance;
+   }
+
+   modifier onlyOwner(){
+    require(msg.sender == owner, "You are not allowed");
+    _;
+   }
+
+  function buySoda() public payable {
+    uint _price = priceOracle.getPrice();
+    require(msg.value == _price , "You must pay 0.01 ether");
+    require(soda > 0, "Sorry, out of stock!");
+    
+    soda = soda - 1;
+}
+
+   function addStock(uint _soda) public onlyOwner{
+    soda = soda + _soda;
+   }
+   
+   function withdraw() public onlyOwner{
+    
+    (bool success, ) = payable(msg.sender).call{value: address(this).balance}("");
+    require(success,"Transfer Failed");
+   }
+
+}
+
